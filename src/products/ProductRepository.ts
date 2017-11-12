36bdfff2a48eaf51e9c7cbcecafadf2e206f4134
Mats Andresen <m.andresen@epages.com>
2017-11-12 17:46:03 +0100

    specify proper return types

--- src/products/ProductRepository.ts
+++ src/products/ProductRepository.ts
@@ -5,17 +5,17 @@ class ProductRepository extends EntityRepository {
         super();
     }
 
-    public async findAll() : Promise<any> {
+    public async findAll() : Promise<Array<Product>> {
         console.log('retrieving all products')
         return await this.query('SELECT * FROM products')
     }
 
-    public async findOne(id : number) {
+    public async findOne(id : number) : Promise<Product> {
         console.log('finding product by id: ' + id)
         return await this.query('SELECT * FROM products WHERE id = ?', [ id ])
     }
 
-    public async save(product : Product) {
+    public async save(product : Product) : Promise<void> {
         console.log('saving product ' + JSON.stringify(product))
         if(product.id) {
             return await this.query('UPDATE products SET sku = ?, name = ?, price = ?, stockLevel = ? WHERE id = ?', [
@@ -35,7 +35,7 @@ class ProductRepository extends EntityRepository {
         }
     }
 
-    public async delete(id : number) {
+    public async delete(id : number) : Promise<void> {
         console.log('deleting product ' + id)
         return await this.query('DELETE FROM products WHERE id = ?', [ id ])
     }
